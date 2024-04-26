611584e189d97756eb76020be20fcfd6bcce0bbc
Alexandr <alexandralexv@gmail.com>
2024-04-27 01:06:31 +0300

    1-module/5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,17 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length < maxlength) {
+    return str;
+  }
+  else {
+    let string = "";
+    for (let i = 0; i < maxlength - 1; i++) {
+      string += str[i];
+    }
+    string += "…"
+    return string;
+  }
 }
+
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 10));
+console.log(truncate('Всем привет!', 20));
