7bf2a91846ad101e28933d28ea794208b17d03aa
Alexandr <alexandralexv@gmail.com>
2024-05-04 20:58:42 +0300

    3-module/2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,8 @@
+// let arr = [5, 3, 8, 1];
+
+// console.log(filterRange(arr, 1, 4)); // [3,1] (совпадающие значения)
+// console.log(arr); // [5,3,8,1] (без изменений)
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(function (item) { return item >= a && item <= b })
 }
