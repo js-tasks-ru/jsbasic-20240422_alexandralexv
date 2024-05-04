705ef928dadded327edd52a6d41587bd2d7aaf6b
Alexandr <alexandralexv@gmail.com>
2024-05-04 21:45:55 +0300

    3-module/3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
+// console.log(camelize('background-color')); // 'backgroundColor';
+// console.log(camelize('list-style-image')); // 'listStyleImage';
+// console.log(camelize('-webkit-transition')); // 'WebkitTransition';
+
 function camelize(str) {
-  // ваш код...
+  let array = str.split('-');
+  for (let i = 1; i < array.length; i++) {
+    array[i] = array[i][0].toUpperCase() + array[i].slice(1);
+  }
+  return array.join('')
 }
