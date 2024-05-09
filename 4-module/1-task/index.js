b1b421ab07bbceb2afecf708cfe63a93e164f1e1
Alexandr <alexandralexv@gmail.com>
2024-05-09 16:33:36 +0300

    4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let body = document.querySelector('body');
+  let ul = document.createElement('UL');
+
+  body.append(ul);
+
+  let mapFriends = friends.map((friend) => `${friend.firstName} ${friend.lastName}`);
+
+  for (let item of mapFriends) {
+    ul.insertAdjacentHTML('afterBegin', `<li>${item}</li>`);
+  }
+  return ul
 }
+
