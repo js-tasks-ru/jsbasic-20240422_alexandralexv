84412e1e50c77d6ba5303a19402647580d83e547
Alexandr <alexandralexv@gmail.com>
2024-05-11 14:03:49 +0300

    start

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,14 @@
 function highlight(table) {
-  // ваш код...
+  let trElements = table.querySelectorAll('tbody tr');
+
+  for (let i = 0; i < trElements.length; i++) {
+    let tdAvailable = trElements[i].querySelector('td[data-available]');
+    console.log(tdAvailable);
+    if (tdAvailable.dataset.available == 'true') {
+      trElements[i].classList.add('available');
+    }
+    else {
+      trElements[i].classList.add('unavailable');
+    }
+  }
 }
