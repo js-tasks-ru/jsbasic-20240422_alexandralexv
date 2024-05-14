50b058204d9d284d9da593f80cc8b734668f127b
Alexandr <alexandralexv@gmail.com>
2024-05-14 22:19:31 +0300

    feat(5-module/2-task): Added function toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.getElementById("text");
+
+  button.onclick = function () {
+    if (text.hidden) {
+      text.hidden = false;
+    }
+    else {
+      text.hidden = true;
+    }
+  }
 }
